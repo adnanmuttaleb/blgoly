6987202743c12947adcdcede391f0227ab34a2cf
adnanmuttaleb <adnanmuttaleb@gmail.com>
2020-07-12 00:44:40 +0300

    add valid

--- articles/resources.py
+++ articles/resources.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Article, Category, Reaction
-from .serializers import ArticleSerializer, CategorySerializer, ReactionSerializer
+from .serializers import ArticleSerializer, CategorySerializer, ReactionSerializer, ReactionToArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -20,14 +20,13 @@ class ReactionViewSet(viewsets.ViewSet):
     serializer_class = ReactionSerializer
 
     def create(self, request):
-        user = request.user
-        article = Article.objects.get(id=request.data["article_id"])
-        type = request.data["type"]
-
-        reaction = Reaction.objects.create(
-            user=user,
-            article=article,
-            type=type
-        )
-
-        return Response(status=status.HTTP_201_CREATED)
+        serializer = ReactionToArticleSerializer(user=request.user, data=request.data)
+        if serializer.is_valid():
+            reaction = Reaction.objects.create(
+                user=request.user,
+                article=serializer.validated_data["article_id"],
+                type=serializer.validated_data["type"]
+            )
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
--- articles/serializers.py
+++ articles/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Article, Reaction, Category
 
 
@@ -18,3 +20,18 @@ class ReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reaction
         fields = ['user', 'article', 'type']
+
+
+class ReactionToArticleSerializer(serializers.Serializer):
+    article_id = serializers.IntegerField()
+    type = serializers.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+    def validate_article_id(self, art_id):
+        try:
+            return Article.objects.get(id=art_id)
+        except:
+            ValidationError("Article Does not Exist")
