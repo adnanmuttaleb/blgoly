908b6123f8c806df0d0fdf178309fa041468d20f
adnanmuttaleb <adnanmuttaleb@gmail.com>
2020-07-12 10:43:09 +0300

    use model + bug

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Article
 
 
-class ArticleList(ListView):
+class ArticleList(LoginRequiredMixin, ListView):
     model = Article
     context_object_name = 'articles'
